4f7344b2ea919846bc27f0e064a8e19791027a33
sanadhis <sanadhis@gmail.com>
2018-03-08 22:33:55 +0100

    --Task1.3 PAX Done

--- src/main/java/ch/epfl/dias/store/PAX/DBPAXpage.java
+++ src/main/java/ch/epfl/dias/store/PAX/DBPAXpage.java
@@ -4,7 +4,53 @@ import ch.epfl.dias.store.DataType;
 import ch.epfl.dias.store.column.DBColumn;
 import ch.epfl.dias.store.row.DBTuple;
 
+import java.util.ArrayList;
+
 public class DBPAXpage {
 
+	public ArrayList<DBColumn> PAXminipages;
+	public DataType[] types;
+	public boolean eof;
 	// TODO: Implement
+
+	public DBPAXpage(Object[][] pagesRecord, DataType[] types, int size){
+		PAXminipages = new ArrayList<DBColumn>();
+		
+		this.types = types;
+
+		int index = 0;
+		for(Object[] pageAttribute: pagesRecord){
+			DBColumn minipage = new DBColumn(pageAttribute, this.types[index++]);
+			PAXminipages.add(minipage);
+		}
+
+		this.eof = true;
+	}
+
+	public DBPAXpage(){
+		this.eof = false;
+	}
+
+	public DBTuple getTuple(int rowNumber){
+		Object[] composedTuple = new Object[types.length];
+		int index = 0;
+		for(DBColumn minipage: PAXminipages){
+			switch(types[index]){
+				case INT:
+					composedTuple[index] = minipage.getAsInteger()[rowNumber];
+					break;
+				case DOUBLE:
+					composedTuple[index] = minipage.getAsDouble()[rowNumber];
+					break;
+				case STRING:
+					composedTuple[index] = minipage.getAsString()[rowNumber];
+					break;
+				case BOOLEAN:
+					composedTuple[index] = minipage.getAsBoolean()[rowNumber];
+					break;
+			}
+			index++;
+		}
+		return new DBTuple(composedTuple, types);
+	}
 }
--- src/main/java/ch/epfl/dias/store/PAX/PAXStore.java
+++ src/main/java/ch/epfl/dias/store/PAX/PAXStore.java
@@ -11,23 +11,84 @@ import java.util.ArrayList;
 import ch.epfl.dias.store.DataType;
 import ch.epfl.dias.store.Store;
 import ch.epfl.dias.store.row.DBTuple;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.util.List;
+
 
 public class PAXStore extends Store {
 
 	// TODO: Add required structures
+	public DataType[] schema;
+	public String filename;
+	public String delimiter;
+	public int tuplesPerPage;
+	public ArrayList<DBPAXpage> paxPages;
 
 	public PAXStore(DataType[] schema, String filename, String delimiter, int tuplesPerPage) {
 		// TODO: Implement
+		this.schema = schema;
+		this.filename = filename;
+		this.delimiter = delimiter;
+		this.tuplesPerPage = tuplesPerPage;
+		this.paxPages = new ArrayList<DBPAXpage>();
 	}
 
 	@Override
 	public void load() {
 		// TODO: Implement
+		String projectPath = System.getProperty("user.dir");
+		Path pathToFile = Paths.get(projectPath + "/" +filename);
+
+		try {
+			InputStream in = Files.newInputStream(pathToFile);
+			BufferedReader reader =	new BufferedReader(new InputStreamReader(in)); 
+			
+			Object[][] genericArr = new Object[schema.length][tuplesPerPage];
+			int counter = 0;
+			String line = null;
+
+			while ((line = reader.readLine()) != null) {
+				String[] data = line.split(delimiter);
+				for (int i=0; i<data.length; i++){
+					Object datapoint = null;
+					switch(schema[i]){
+						case INT:
+							datapoint = Integer.parseInt(data[i]);
+							break;
+						case DOUBLE:
+							datapoint = Double.parseDouble(data[i]);
+							break;
+						case STRING:
+							datapoint = data[i];
+							break;
+						case BOOLEAN:
+							datapoint = Boolean.parseBoolean(data[i]);
+							break;
+					}
+					genericArr[i][counter] = datapoint;
+				}
+				counter++;
+				if (counter == tuplesPerPage){
+					paxPages.add(new DBPAXpage(genericArr, schema, tuplesPerPage));
+					genericArr = new Object[schema.length][tuplesPerPage];
+					counter = 0;
+				}
+			}
+			if (counter != 0){
+				paxPages.add(new DBPAXpage(genericArr, schema, tuplesPerPage));
+			}
+		} catch (IOException x) {
+			System.err.println(x);
+		}
 	}
 
 	@Override
 	public DBTuple getRow(int rownumber) {
-		// TODO: Implement
-		return null;
+		int page = rownumber / tuplesPerPage ;
+		int rowPosition = rownumber % tuplesPerPage ;
+		DBTuple rowResult = paxPages.get(page).getTuple(rowPosition);
+		return rowResult;
 	}
 }
