a63054d3f533498e968bd9d9655a52d3ce0e4dee
sanadhis <sanadhis@gmail.com>
2018-03-13 22:58:02 +0100

    --Task2.3: Add Base Scan and Select Implementation

--- src/main/java/ch/epfl/dias/ops/vector/Scan.java
+++ src/main/java/ch/epfl/dias/ops/vector/Scan.java
@@ -6,24 +6,73 @@ import ch.epfl.dias.store.column.DBColumn;
 public class Scan implements VectorOperator {
 
 	// TODO: Add required structures
+	private Store store;
+	private int vectorSize;
+	private int vectorIndex;
+	private DBColumn[] allColumns;
+	private boolean eofFlag;
 
-	public Scan(Store store) {
+	public Scan(Store store, int vectorSize) {
 		// TODO: Implement
+		this.store = store;
+		this.vectorSize = vectorSize;
+		this.allColumns = null;
+		this.eofFlag = false;
 	}
 	
 	@Override
 	public void open() {
 		// TODO: Implement
+		vectorIndex = 0;
+		allColumns = store.getColumns(new int[]{});
 	}
 
 	@Override
 	public DBColumn[] next() {
 		// TODO: Implement
-		return null;
+		if(eofFlag){
+			return null;
+		}
+		else{
+			DBColumn[] nextVector = new DBColumn[allColumns.length];
+			int index = 0;
+			for(DBColumn column: allColumns){
+				Object[] result = null;
+				switch(column.getDataType()){
+					case INT:
+						result = column.getAsInteger();
+						break;
+					case DOUBLE:
+						result = column.getAsDouble();
+						break;
+					case STRING:
+						result = column.getAsString();
+						break;
+					case BOOLEAN:
+						result = column.getAsBoolean();
+						break;
+				}
+				Object[] blockResult = new Object[vectorSize];
+				for (int i=0; i<vectorSize; i++){
+					try{
+						blockResult[i] = result[vectorIndex+i];
+					}
+					catch(IndexOutOfBoundsException e){
+						blockResult[i] = null;
+						eofFlag = true;
+					}
+				}
+				nextVector[index++] = new DBColumn(blockResult, column.getDataType());			
+			}
+			vectorIndex += vectorSize;		
+			return nextVector;
+		}
 	}
 
 	@Override
 	public void close() {
 		// TODO: Implement
+		vectorIndex = 0;
+		store = null;
 	}
 }
--- src/main/java/ch/epfl/dias/ops/vector/Select.java
+++ src/main/java/ch/epfl/dias/ops/vector/Select.java
@@ -3,27 +3,106 @@ package ch.epfl.dias.ops.vector;
 import ch.epfl.dias.ops.BinaryOp;
 import ch.epfl.dias.store.column.DBColumn;
 
+import java.util.ArrayList;
+
 public class Select implements VectorOperator {
 
 	// TODO: Add required structures
+	private VectorOperator child;
+	private BinaryOp op;
+	private int fieldNo;
+	private int value;
 
 	public Select(VectorOperator child, BinaryOp op, int fieldNo, int value) {
 		// TODO: Implement
+		this.child = child;
+		this.op = op;
+		this.fieldNo = fieldNo;
+		this.value = value;
 	}
 	
 	@Override
 	public void open() {
 		// TODO: Implement
+		child.open();
 	}
 
 	@Override
 	public DBColumn[] next() {
 		// TODO: Implement
-		return null;
+		DBColumn[] childVector = child.next();
+		DBColumn[] vectorSelection = new DBColumn[childVector.length];
+		ArrayList<DBColumn> selectionResult = new ArrayList<DBColumn>();
+		ArrayList<Integer> selectionIndex = new ArrayList<Integer>();
+		int index = 0;
+		for (Integer val: childVector[fieldNo].getAsInteger()){
+			if(val == null){
+				break;
+			}
+			switch(op){
+				case LT:
+					if(val < value){
+						selectionIndex.add(index);
+					}
+					break;
+				case LE:
+					if(val <= value){
+						selectionIndex.add(index);
+					}
+					break;
+				case EQ:
+					if(val == value){
+						selectionIndex.add(index);
+					}
+					break;
+				case NE:
+					if(val != value){
+						selectionIndex.add(index);
+					}
+					break;
+				case GT:
+					if(val > value){
+						selectionIndex.add(index);
+					}
+					break;
+				case GE:
+					if(val >= value){
+						selectionIndex.add(index);
+					}
+					break;
+			}
+			index++;			
+		}
+		index = 0;
+		for(DBColumn block: childVector){
+			Object[] result = null;
+			switch(block.getDataType()){
+				case INT:
+					result = block.getAsInteger();
+					break;
+				case DOUBLE:
+					result = block.getAsDouble();
+					break;
+				case STRING:
+					result = block.getAsString();
+					break;
+				case BOOLEAN:
+					result = block.getAsBoolean();
+					break;
+			}
+			Object[] blockResult = new Object[selectionIndex.size()];
+			for (int i=0;i<selectionIndex.size();i++){
+				blockResult[i] = result[selectionIndex.get(i)];
+			}
+			vectorSelection[index++] = new DBColumn(blockResult, block.getDataType());
+		}
+		
+		return vectorSelection;
 	}
 
 	@Override
 	public void close() {
 		// TODO: Implement
+		child.close();
 	}
 }
